535a299dda716f0fce72c9c3a82079e7581814ae
DL <ixxi@xx-ll.ru>
2026-04-17 02:29:40 +0300

    fix creating tables

--- app/main.py
+++ app/main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 from api import router
 from contextlib import asynccontextmanager
 from core.database import engine
-from models import Base
+from models import Base, models, service
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all, tables=[models.User.__table__,
+                                                              models.Order.__table__,
+                                                              models.AdditionalWork.__table__,
+                                                              service.Service.__table__,
+                                                              service.Photo.__table__])
     yield
     await engine.dispose()
 
