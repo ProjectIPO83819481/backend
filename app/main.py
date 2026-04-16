69e1bfafcb710f60f9eddcf0197e6e49037ed0e0
DL <ixxi@xx-ll.ru>
2026-04-17 02:18:34 +0300

    fix `regex` has been deprecated

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 
 from api import router
+from contextlib import asynccontextmanager
+from core.database import engine
+from models import Base
 
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
 
 app = FastAPI(title='Customer Service API',
               version='1.0b',
               root_path="/v1",
               redoc_url='/',
               docs_url=None,
+              lifespan=lifespan
               )
 app.include_router(router)
 
