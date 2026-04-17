bbe4f25b2850cebad867bbd7d800da164144f807
thekorp <146642604+queolvi@users.noreply.github.com>
2026-04-17 05:33:54 +0300

    add simple frontend
    Signed-off-by: thekorp <146642604+queolvi@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+
+from app.api.v1.router import router
+from contextlib import asynccontextmanager
+from app.core.database import engine
+from app.models.models import User, Order, AdditionalWork
+from app.models.service import Service, Photo
+from app.models.base import Base
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all, tables=[User.__table__,
+                                                              Order.__table__,
+                                                              AdditionalWork.__table__,
+                                                              Service.__table__,
+                                                              Photo.__table__])
+    yield
+    await engine.dispose()
+
+app = FastAPI(
+    title='Customer Service API',
+    version='1.0b',
+    redoc_url='/',
+    docs_url='/docs',
+    lifespan=lifespan
+)
+app.include_router(router, prefix="/v1")
+
+from fastapi.staticfiles import StaticFiles
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=8085)
